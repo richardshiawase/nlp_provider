2c6ca0ef74ff0dc95ff58d6dd272b44f2f33de3c
Richard Shiawase <richard.shiawase@andika.co.id>
2022-12-13 10:51:54 +0700

    wq

--- nlp_provider/settings.py
+++ nlp_provider/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'model',
+    'nlp_provider',
 ]
 
 MIDDLEWARE = [
@@ -118,9 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'nlp_provider/static')]
+STATIC_URL = 'static/'
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'nlp_provider/static/')]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = "home"
